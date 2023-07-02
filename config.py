d6885b4f80df38364283f121ae8ece9393419e57
khushveer007 <122660325+khushveer007@users.noreply.github.com>
2023-07-02 22:59:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     API_HASH = os.environ.get("9758292b691b82c5915900c506fcbe83")
     BOT_TOKEN = os.environ.get("5848537364:AAEwt5ED7zOcTYOHBJdn1jMmJ1GJ8po3rv4")
-    TELEGRAM_API = os.environ("26367674")
+    TELEGRAM_API = os.environ.get("26367674")
     OWNER = os.environ.get("5697445137")
     OWNER_USERNAME = os.environ.get("Khushveersinghdeora")
     PASSWORD = os.environ.get("qwerty1234")
